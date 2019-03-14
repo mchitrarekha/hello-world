987f77237920c07ad20be6c7f9647a80995b8eb9
Chitrarekha <chitrarekha@jai-kisan.com>
2019-03-14 16:00:48 +0530

    testing webhook

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+console.log('hello world')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
